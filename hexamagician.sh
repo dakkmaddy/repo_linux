6227ab59b1223629b24c430f3189861db2ae833f
dakkmaddy <42662940+dakkmaddy@users.noreply.github.com>
2019-06-30 21:05:29 -0600

    Update hexamagician.sh
    Added comments for the next part

--- hexamagician.sh
+++ hexamagician.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # hexamagician, helps you with hexadecimal math
@@ -15,7 +16,7 @@ standard=(01 02 03 04 05 06 07 08 09 11 12 14 15 16 17 18 19 20 21 22 23 24 25 2
 if [ ! -f $1 ]; then
         echo "[-] Cannot find your file!"
         echo "[-] Script needs a single argument of the full path to the shellcode file"
-	echo "[-] Exiting"
+        echo "[-] Exiting"
         exit
 fi
 #
@@ -52,37 +53,37 @@ do
 # if eo1 is 1 to 5, we choose one of the preformed options and go from there
 # if eo1 is something else, we exit
 #
-	if [[ $eo1 -gt 0 && $eo1 -lt 6 ]]
-	then
-		iv1="valid"
-	fi
-	if [[ $eo1 -eq 6 ]]
+        if [[ $eo1 -gt 0 && $eo1 -lt 6 ]]
+        then
+                iv1="valid"
+        fi
+        if [[ $eo1 -eq 6 ]]
         then
                 echo
                 echo "[-] Enter custom list here. Enter each hex bad char as two digits, separted by a space."
-		echo "[-] Use CAPS. This will render as an array "
-		echo "[-] Data validation is not in use, so if you screw it up, it is to your own peril"
-		read -a customlist1
-		for loop1 in "${customlist1[@]}"
-		do
-			# Need to convert hex to decimal real quick
-			# Will use obase
-			purgeme=$(echo "obase=10; ibase=16; $loop1" | bc)
-			unset all[$purgeme]
-		done
-		# Test command to see if the all array has been modified.
-		# Appears so, JMM 6/25/2019
-		#declare -p all
+                echo "[-] Use CAPS. This will render as an array "
+                echo "[-] Data validation is not in use, so if you screw it up, it is to your own peril"
+                read -a customlist1
+                for loop1 in "${customlist1[@]}"
+                do
+                        # Need to convert hex to decimal real quick
+                        # Will use obase
+                        purgeme=$(echo "obase=10; ibase=16; $loop1" | bc)
+                        unset all[$purgeme]
+                done
+                # Test command to see if the all array has been modified.
+                # Appears so, JMM 6/25/2019
+                #declare -p all
                 iv1="valid"
         fi
-	if [[ $eo1 -gt 6 ]]
-	then
-		echo "[-] Invalid input, .. try again dude"
-		# Putting exit, I do not feel like moving the while statement and reindexing everythin
-		# Either use the script correctly or I am exiting
-		exit
-	fi
-done 
+        if [[ $eo1 -gt 6 ]]
+        then
+                echo "[-] Invalid input, .. try again dude"
+                # Putting exit, I do not feel like moving the while statement and reindexing everythin
+                # Either use the script correctly or I am exiting
+                exit
+        fi
+done
 echo "[-] Input validated, proceeding .."
 echo
 sleep 2
@@ -94,36 +95,36 @@ sleep 2
 #
 if [[ $eo1 -eq 1 ]]
 then
-	activearray=( "${all[@]}" )
-	# This is all characters allowed (rare)
-	# We can use xor eax, eax
-	usexor="yes"
+        activearray=( "${all[@]}" )
+        # This is all characters allowed (rare)
+        # We can use xor eax, eax
+        usexor="yes"
 # For options 2 through 4 lower, upper and alpha we do not have both 31 and C0
 # usexor will be NO
 elif [[ $eo1 -eq 2 ]]
 then
- 	activearray=( "${lower[@]}" )
-	usexor="no"
+        activearray=( "${lower[@]}" )
+        usexor="no"
 elif [[ $eo1 -eq 3 ]]
 then
-	usexor="no"
-	activearray=( "${upper[@]}" )
+        usexor="no"
+        activearray=( "${upper[@]}" )
 elif [[ $eo1 -eq 4 ]]
 then
-	usexor="no"
-	activearray=( "${alpha[@]}" )
+        usexor="no"
+        activearray=( "${alpha[@]}" )
 elif [[ $eo1 -eq 5 ]]
 then
-	usexor="yes"
-	activearray=( "${standard[@]}" )
+        usexor="yes"
+        activearray=( "${standard[@]}" )
 elif [[ $eo1 -eq 6 ]]
 then
-	# Will check below before dealing with usexor
-	#echo "[-] Array previously assigned ... "
-	activearray=( "${all[@]}" )
+        # Will check below before dealing with usexor
+        #echo "[-] Array previously assigned ... "
+        activearray=( "${all[@]}" )
 else
-	echo "[-] You can't see me!"
-	echo "[-] You can't even get here"
+        echo "[-] You can't see me!"
+        echo "[-] You can't even get here"
 fi
 echo "[-] Active Array set!"
 # test echo
@@ -139,39 +140,39 @@ cat $1 | tr -d '\n' | sed 's/\\x//g' | sed 's/\"//g' | sed 's/;//g' | sed 's/ //
 #echo -n "[-] Converted as .. " && cat /tmp/shellcode_parse_1.txt
 #exit
 echo "[-] Shellcode converted to binary paste!"
-# 
+#
 # time to select a ZERO EAX strategy
 # If we can use opcode 31 c0 for xor eax, eax
 # We need to see if opcodes exist in the activearray (array)
 # We can do this with nested if then (else) fi
 if [[ $eo1 -eq 6 ]];
 then
-	# First set usexor to no. If it passes all tests, it gets changed to yes
-	usexor="no"
-	echo "[-] Parsing Array for opcodes 31 C0 (XOR EAX, EAX)"
-	for opcodecheck in "${activearray[@]}"
-	do
-	if [ "$opcodecheck" == 49 ];
-	then
-		# The activearray variable contains 31. We can use that opcode. Need to check for C0
-		# Need another for do done loop to check the bit set
-		# This is the only way for eo6 (encoding option 6) to get usexor = yes
-		echo "[-] 31 (HEX) found, checking for C0, For loop 2"
-		for opcodecheck2 in "${activearray[@]}"
-		do
-			if [[ "$opcodecheck2" == 192 ]];
-			then
-				# Both conditions are met. We can use xor eax, eax in our shellcode
-				usexor="yes"
-				echo "[-] We will be able to use xor eax, eax"
-				echo "[-] Isn't that wonderful!"
-			fi
-		done	
-	fi
-	done
+        # First set usexor to no. If it passes all tests, it gets changed to yes
+        usexor="no"
+        echo "[-] Parsing Array for opcodes 31 C0 (XOR EAX, EAX)"
+        for opcodecheck in "${activearray[@]}"
+        do
+        if [ "$opcodecheck" == 49 ];
+        then
+                # The activearray variable contains 31. We can use that opcode. Need to check for C0
+                # Need another for do done loop to check the bit set
+                # This is the only way for eo6 (encoding option 6) to get usexor = yes
+                echo "[-] 31 (HEX) found, checking for C0, For loop 2"
+                for opcodecheck2 in "${activearray[@]}"
+                do
+                        if [[ "$opcodecheck2" == 192 ]];
+                        then
+                                # Both conditions are met. We can use xor eax, eax in our shellcode
+                                usexor="yes"
+                                echo "[-] We will be able to use xor eax, eax"
+                                echo "[-] Isn't that wonderful!"
+                        fi
+                done
+        fi
+        done
 else
-	# usexor variable previously set
-	echo "[-] YOU CAN'T SEE ME!" >/dev/null
+        # usexor variable previously set
+        echo "[-] YOU CAN'T SEE ME!" >/dev/null
 fi
 #
 #echo "[-] Checked for XOR use complete "
@@ -181,44 +182,58 @@ echo
 # Quick if then else fi to let user know approx shellcode size
 if [ $usexor = "yes" ];
 then
-	echo "[-] We will be able to use XOR EAX, EAX, smaller shellcode!"
+        echo "[-] We will be able to use XOR EAX, EAX, smaller shellcode!"
 else
-	echo "[-] We are using the AND method to zero out EAX. Larger shellcode"
-fi 
+        echo "[-] We are using the AND method to zero out EAX. Larger shellcode"
+fi
 # Now to iterate through each line in the tmp file and convert our shellcode
 cat /tmp/shellcode_parse_1.txt
 echo "[-] Parsing shellcode file!"
 while read hexvaluetext
 do
-	# Do lots of stuff
-	# Step 1, work the ZERO function
-	# Will use a file called "/tmp/running_shellcode.txt"
-	#
-	running="/tmp/running_shellcode.txt"
-	if [ $usexor = "yes" ]
-	then
-		echo -n "\x31\x50" >> $running
-	else
-		echo -n "\x25\x4A\x4D\x4E\x55\x25\x35\x32\x31\x23\x54\x58" >> $running
-	fi
-	#
-	# Our shellcode string from the parse file needs to be converted to a number
-	# So bash can make the right decision based on math
-	# Variable makeupper ensures the parsed line is in "HEX"
-	# Variable makedecimal converts it to a number in decimal
-	makeupper=$(echo $hexvaluetext | tr "[abcdef]" "[ABCDEF]")
-	makedecimal=$(echo "obase=10; ibase=16; $makeupper" | bc)
-	echo $makeupper >> $running
-	# Now that it has been converted to decimal, I can calculate the new numbers and work on the triggers for 2/3 operands math
-	# Reason
-	# What is 100000000 (Hex) in decimal?
-	# Answer : 4294967296
-	# Everytime
-	#
-	# We can use this to define limits for the encoding strategy
-	# Calculting numbers for decimal and hextarget. Will be same.
-	#decimaltarget=(( 4294967296 - $makedecimal ))
-	#hextarget=$(echo "obase=16; ibase=10; $decimaltarget | bc)
+        # Do lots of stuff
+        # Step 1, work the ZERO function
+        # Will use a file called "/tmp/running_shellcode.txt"
+        #
+        running="/tmp/running_shellcode.txt"
+        # Use the usexor variable to AND EAX (one way or another)
+        if [ $usexor = "yes" ]
+        then
+                echo -n "\x31\x50" >> $running
+        else
+                echo -n "\x25\x4A\x4D\x4E\x55\x25\x35\x32\x31\x23\x54\x58" >> $running
+        fi
+        # XOR eax, eax added to "manually encoded shellcode"
+        #
+        # Our shellcode string from the parse file needs to be converted to a number
+        # So bash can make the right decision based on math
+        # Variable makeupper ensures the parsed line is in "HEX"
+        # Variable makedecimal converts it to a number in decimal
+        makeupper=$(echo $hexvaluetext | tr "[abcdef]" "[ABCDEF]")
+        makedecimal=$(echo "obase=10; ibase=16; $makeupper" | bc)
+        echo $makeupper >> $running
+        # Now that it has been converted to decimal, I can calculate the new numbers and work on the triggers for 2/3 operands math
+        # Reason
+        # What is 100000000 (Hex) in decimal?
+        # Answer : 4294967296
+        # Everytime
+        #
+        # Now we need the math target
+        # This is the number 4294967296 - $makedecimal.
+        # This value and the eo(x) NOT ARRAY, encoding option
+        # Will determine how many iterations we subtract to carve or value int EAX
+        # And we need to use the array to avoid badcharacters
+        # Range defs
+        # halfrange = 7f7f7f7f is 2139062143 in decimal
+        # If the intended value for EAX is greater than halfrange
+        # Our subtrahends can be a single value less than halfrange for
+        # eo1, eo2, eo4 ,e05 and e06
+        # e03 requires a second subtrahend
+        # quadrange = 7f7f7f is 8355711 in decimal
+        # If intended value is less than quadrange
+        # Then 1st subtrahend is halfrange. Second is 7fxyxyxy
+        # 7f7f is 32639 in decimal
+
 done < /tmp/shellcode_parse_1.txt
 #
 # New shellcode
